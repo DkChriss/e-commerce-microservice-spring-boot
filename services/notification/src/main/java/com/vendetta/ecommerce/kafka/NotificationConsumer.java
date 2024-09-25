9952b7bb3ef0899ecde67db03086131b6f0605e7
Christopher Huayhua <chuanca@cochabamba.bo>
2024-09-25 17:08:55 -0400

    Create notification service

---
+++ services/notification/src/main/java/com/vendetta/ecommerce/kafka/NotificationConsumer.java
@@ -0,0 +1,71 @@
+package com.vendetta.ecommerce.kafka;
+
+import com.vendetta.ecommerce.email.EmailService;
+import com.vendetta.ecommerce.kafka.order.OrderConfirmation;
+import com.vendetta.ecommerce.kafka.payment.PaymentConfirmation;
+import com.vendetta.ecommerce.notification.Notification;
+import com.vendetta.ecommerce.notification.NotificationRepository;
+import com.vendetta.ecommerce.notification.NotificationType;
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+import static com.vendetta.ecommerce.notification.NotificationType.ORDER_CONFIRMATION;
+import static com.vendetta.ecommerce.notification.NotificationType.PAYMENT_CONFIRMATION;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class NotificationConsumer {
+
+    private final NotificationRepository repository;
+    private final EmailService emailService;
+
+
+    @KafkaListener(topics = "payment-topic")
+    public void consumePaymentSuccessNotification(PaymentConfirmation paymentConfirmation) throws MessagingException {
+        log.info(String.format("Consuming the message from payment-topic Topic:: %s", paymentConfirmation));
+        repository.save(
+                Notification.builder()
+                        .type(PAYMENT_CONFIRMATION)
+                        .notificationDate(LocalDateTime.now())
+                        .paymentConfirmation(paymentConfirmation)
+                        .build()
+        );
+
+        var customerName = paymentConfirmation.customerFirstName() + " " + paymentConfirmation.customerLastName();
+        emailService.sendPaymentSuccessEmail(
+                paymentConfirmation.customerEmail(),
+                customerName,
+                paymentConfirmation.amount(),
+                paymentConfirmation.orderReference()
+        );
+    }
+
+    @KafkaListener(topics = "order-topic")
+    public void consumeOrderConfirmNotification(OrderConfirmation orderConfirmation) throws MessagingException {
+        log.info(String.format("Consuming the message from payment-topic Topic:: %s", orderConfirmation));
+        repository.save(
+                Notification.builder()
+                        .type(ORDER_CONFIRMATION)
+                        .notificationDate(LocalDateTime.now())
+                        .orderConfirmation(orderConfirmation)
+                        .build()
+        );
+
+        var customerName = orderConfirmation.customer().firstname() + " " + orderConfirmation.customer().lastname();
+        emailService.sendOrderConfirmationEmail(
+                orderConfirmation.customer().email(),
+                customerName,
+                orderConfirmation.totalAmount(),
+                orderConfirmation.orderReference(),
+                orderConfirmation.products()
+        );
+    }
+
+
+}
---
+++ services/notification/src/main/java/com/vendetta/ecommerce/kafka/order/OrderConfirmation.java
@@ -0,0 +1,15 @@
+package com.vendetta.ecommerce.kafka.order;
+
+import com.vendetta.ecommerce.kafka.payment.PaymentMethod;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public record OrderConfirmation(
+        String orderReference,
+        BigDecimal totalAmount,
+        PaymentMethod paymentMethod,
+        Customer customer,
+        List<Product> products
+) {
+}
